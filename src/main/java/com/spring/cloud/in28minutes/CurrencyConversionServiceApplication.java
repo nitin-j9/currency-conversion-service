a124698ceaac6b06684c5fdf60d64ba4934a58d0
Nitin.Jain <Nitin.Jain@192.168.29.251>
2021-12-31 17:54:24 +0530

    New changes

--- src/main/java/com/spring/cloud/in28minutes/CurrencyConversionServiceApplication.java
+++ src/main/java/com/spring/cloud/in28minutes/CurrencyConversionServiceApplication.java
@@ -2,12 +2,14 @@ package com.spring.cloud.in28minutes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.spring.cloud.in28minutes")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
